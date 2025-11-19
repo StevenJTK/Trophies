c1afb04d0756e7df01f101608dd64453252c774c
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2025-11-19 10:35:07 +0100

    Initial Commit

---
+++ src/main/java/com/sti/steven/trophies/TrophiesApplication.java
@@ -0,0 +1,13 @@
+package com.sti.steven.trophies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrophiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrophiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sti/steven/trophies/TrophiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sti.steven.trophies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrophiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
