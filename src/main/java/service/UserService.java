274dee22b417f2753d72111879988786a24e4571
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2025-11-20 14:40:08 +0100

    Add: Find Username and Email methods.

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,6 +2,7 @@ package service;
 
 import interfaces.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import product.Role;
 import product.User;
@@ -14,12 +15,12 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Autowired
-    public UserService(RoleRepository roleRepository, UserRepository userRepoitory) {
+    public UserService(RoleRepository roleRepository, UserRepository userRepository) {
         this.roleRepository = roleRepository;
-        this.userRepository = userRepoitory;
+        this.userRepository = userRepository;
     }
 
-    public  User addUser(User user) {
+    public User createNewUser(User user) {
         if (user == null) {
             throw new IllegalArgumentException("User cannot be null.");
         }
@@ -27,10 +28,10 @@ public class UserService {
     }
 
     public void giveRoleToUser(User user, Role role) {
-        if(user == null || role == null) {
+        if (user == null || role == null) {
             throw new IllegalArgumentException("User cannot be null.");
         }
-        if(!user.getRoles().contains(role)) {
+        if (!user.getRoles().contains(role)) {
             user.getRoles().add(role);
             userRepository.save(user);
         }
@@ -41,11 +42,42 @@ public class UserService {
             throw new IllegalArgumentException("User or role cannot be null.");
         }
 
-        if(!user.getRoles().contains(role)) {
+        if (!user.getRoles().contains(role)) {
             user.getRoles().remove(role);
             userRepository.save(user);
         } else {
             throw new IllegalArgumentException("User does not have this role.");
         }
+    }
+
+    public User findByUsername(String username) {
+        User user;
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null.");
+        }
+        user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User was not found.");
+        }
+
+        return user;
+    }
+
+    public User findByEmail(String email) {
+        User user;
+
+        if (email == null) {
+            throw new IllegalArgumentException("Email cannot be null.");
+        }
+
+        user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User was not found.");
+        }
+
+        return user;
     }
 }
