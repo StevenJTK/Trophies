bb109d8431a70abc9f9a87aaedb0762ccd8b72ca
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2025-11-28 11:24:32 +0100

    Add: Controller Mapping

--- src/main/java/com/sti/steven/trophies/controller/UserController.java
+++ src/main/java/com/sti/steven/trophies/controller/UserController.java
@@ -1,30 +1,33 @@
 package com.sti.steven.trophies.controller;
 
+import com.sti.steven.trophies.interfaces.UserRepository;
+import com.sti.steven.trophies.product.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
 
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String helloUser() {
-        return "Hello User.";
+    UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User userRegistered = userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRegistered);
     }
 
+    @GetMapping("/userDetails/{id}")
+    public ResponseEntity<User> userDetails(@PathVariable int id) {
+        Optional<User> userOptional = userRepository.findById(id);
 
-    @RequestMapping("/trophies")
-    @ResponseBody
-    public String displayTrophies() {
-        return "Your trophies should display here";
-    }
-
-
-    @RequestMapping("admin")
-    @ResponseBody
-    public String displayAdminPage() {
-        return "This is actually a super secret admin page";
+        return userOptional
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
