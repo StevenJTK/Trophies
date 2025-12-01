737b9171ab043bd5de121743c8feafa42f8c9ebc
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2025-12-01 16:40:06 +0100

    Debuggin

--- src/main/java/com/sti/steven/trophies/controller/AuthController.java
+++ src/main/java/com/sti/steven/trophies/controller/AuthController.java
@@ -28,7 +28,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
--- src/main/java/com/sti/steven/trophies/controller/UserController.java
+++ src/main/java/com/sti/steven/trophies/controller/UserController.java
@@ -1,17 +1,12 @@
 package com.sti.steven.trophies.controller;
 
-import com.sti.steven.trophies.dto.UserDTO;
+
 import com.sti.steven.trophies.interfaces.UserRepository;
-import com.sti.steven.trophies.product.User;
 import com.sti.steven.trophies.service.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/user/register")
